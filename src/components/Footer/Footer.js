67cc2e3f21775dbf7107f59e2577c0a91ecfa08a
Seva <sevauni@gmail.com>
2023-01-07 21:13:56 +0200

    first layout

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css'
+import Button from 'react-bootstrap/Button';
+
+
+const Footer = () => {
+  return (
+    <footer className='footer'>
+      <div className='test'>
+sdfsdf
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ src/components/MainMenu/MainMenu.js
@@ -0,0 +1,16 @@
+import './MainMenu.css';
+import Logo from '../../img/icon.svg';
+
+
+const MainMenu = () => {
+  return (
+    <nav className='mainmenu'>
+
+
+        <img src={Logo} alt="Logo" />
+
+    </nav>
+  );
+}
+
+export default MainMenu;
