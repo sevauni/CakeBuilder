88abcc79e55d7921e9b35b9525681e202d8a7fbd
Seva <sevauni@gmail.com>
2023-01-09 17:56:34 +0200

    i cannot believe this is cake

---
+++ src/components/CakeBuilder/CakeLayer/CakeLayer.js
@@ -0,0 +1,21 @@
+function CakeLayer() {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 196.667 42.659">
+        <g stroke="#000" strokeLinecap="round" strokeMiterlimit="4.9">
+          <path
+            fill="#e6e6e6"
+            strokeWidth="2.008"
+            d="M-30.116-168.062H215.16v252.63H-30.116z"
+          ></path>
+          <path
+            fill="#fcdeb0"
+            strokeLinejoin="round"
+            strokeWidth="7.975"
+            d="M3.987 3.988H192.68v34.684H3.987z"
+          ></path>
+        </g>
+      </svg>
+    );
+  }
+
+  export default CakeLayer;
