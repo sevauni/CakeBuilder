04a13359b36066e65004c9878666dbdb285c817c
Seva <sevauni@gmail.com>
2023-01-11 21:16:54 +0200

    add github link

--- src/components/MainMenu/MainMenu.js
+++ src/components/MainMenu/MainMenu.js
@@ -1,14 +1,21 @@
 import './MainMenu.css';
 import Logo from '../../img/icon.svg';
-
+import GitHubButton from 'react-github-btn'
 
 const MainMenu = () => {
   return (
     <nav className='mainmenu'>
 
 
-        <img src={Logo} alt="Logo" />
-        <h1>The Little Bakery</h1>
+      <img src={Logo} alt="Logo" />
+      <h1>The Little Bakery</h1>
+      <div className='github-btn-menu'>
+        <GitHubButton href="https://github.com/sevauni"
+          data-size="large"
+          aria-label="Follow @sevauni on GitHub"
+        >Follow @sevauni</GitHubButton>
+      </div>
+
 
     </nav>
   );
